50db5e28c96202bfff5cdac2d50292db84395392
piperod <ivan.rodriguez5@upr.edu>
2020-11-23 10:28:36 -0500

    Adding folder tagging for folder inference

--- pytorch/inference.py
+++ pytorch/inference.py
@@ -11,6 +11,8 @@ from utilities import create_folder, get_filename
 from models import *
 from pytorch_utils import move_data_to_device
 import config
+import glob
+import json
 
 
 def audio_tagging(args):
@@ -75,6 +77,81 @@ def audio_tagging(args):
         embedding = batch_output_dict['embedding'].data.cpu().numpy()[0]
         print('embedding: {}'.format(embedding.shape))
 
+    return clipwise_output, labels
+
+def audio_tagging_folder(args):
+    """Inference audio tagging result of an audio clip.
+    """
+
+    # Arugments & parameters
+    folder = args.folder
+    
+    sample_rate = args.sample_rate
+    window_size = args.window_size
+    hop_size = args.hop_size
+    mel_bins = args.mel_bins
+    fmin = args.fmin
+    fmax = args.fmax
+    model_type = args.model_type
+    
+    #audio_path = args.audio_path
+    device = torch.device('cuda') if args.cuda and torch.cuda.is_available() else torch.device('cpu')
+    
+    classes_num = config.classes_num
+    labels = config.labels
+    aud_paths = glob.glob(os.path.join(folder,'*wav'))+glob.glob(os.path.join(folder,'*mp3'))
+    
+    # Model
+    Model = eval(model_type)
+    model = Model(sample_rate=sample_rate, window_size=window_size, 
+            hop_size=hop_size, mel_bins=mel_bins, fmin=fmin, fmax=fmax, 
+            classes_num=classes_num)
+    
+    checkpoint_path = args.checkpoint_path
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(checkpoint['model'])
+    
+    # Parallel
+    if 'cuda' in str(device):
+        model.to(device)
+        print('GPU number: {}'.format(torch.cuda.device_count()))
+        model = torch.nn.DataParallel(model)
+    else:
+        print('Using CPU.')
+    
+    results ={}
+    for audio_path in aud_paths:
+        print('Infering path:',audio_path)
+        # Load audio
+        (waveform, _) = librosa.core.load(audio_path, sr=sample_rate, mono=True)
+
+        waveform = waveform[None, :]    # (1, audio_length)
+        waveform = move_data_to_device(waveform, device)
+        # Forward
+        with torch.no_grad():
+            model.eval()
+            batch_output_dict = model(waveform, None)
+
+        clipwise_output = batch_output_dict['clipwise_output'].data.cpu().numpy()[0]
+        """(classes_num,)"""
+
+        sorted_indexes = np.argsort(clipwise_output)[::-1]
+
+        # Print audio tagging top probabilities
+        result = {}
+        for k in range(10):
+            result[str(np.array(labels)[sorted_indexes[k]])]= float(clipwise_output[sorted_indexes[k]])
+            print('{}: {:.3f}'.format(np.array(labels)[sorted_indexes[k]], 
+                clipwise_output[sorted_indexes[k]]))
+        results[audio_path]=result
+        # Print embedding
+        if 'embedding' in batch_output_dict.keys():
+            embedding = batch_output_dict['embedding'].data.cpu().numpy()[0]
+            print('embedding: {}'.format(embedding.shape))
+    
+    out_name = os.path.join(folder,'results.json')
+    with open(out_name, 'w') as fp:
+        json.dump(results, fp)
     return clipwise_output, labels
 
 
@@ -182,6 +259,7 @@ if __name__ == '__main__':
     parser_at.add_argument('--checkpoint_path', type=str, required=True)
     parser_at.add_argument('--audio_path', type=str, required=True)
     parser_at.add_argument('--cuda', action='store_true', default=False)
+    parser_at.add_argument('--folder',type=str,default=None)
 
     parser_sed = subparsers.add_parser('sound_event_detection')
     parser_sed.add_argument('--sample_rate', type=int, default=32000)
@@ -198,10 +276,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.mode == 'audio_tagging':
-        audio_tagging(args)
+        if args.folder:
+            audio_tagging_folder(args)
+        else:
+            audio_tagging(args)
 
     elif args.mode == 'sound_event_detection':
         sound_event_detection(args)
-
+    
     else:
         raise Exception('Error argument!')
